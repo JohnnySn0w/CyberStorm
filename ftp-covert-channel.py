70338fde205f60769dacc203dc41446b57214b21
Samantha Santiago <26655231+DJSanti@users.noreply.github.com>
2018-03-27 15:25:30 -0500

    Add files via upload
    Just a blank program with comment outlining what we need to get done.

---
+++ ftp-covert-channel.py
@@ -0,0 +1,22 @@
+######################################################
+# Group: Greed
+# Members: Alexandra Duran Chicas, Zachary Guillot, 
+# Conan Howard, Oluwatoyosi Kade, Michael Mahan, 
+# Darryl Rayborn, Jonathan Ruppel, Samantha Santiago
+# Date: 03/27/2018
+# Program:
+# Objective:
+######################################################
+
+
+# this can either all be in one main function, or have one main and separate functions
+
+# things that need to get done:
+
+# fetch file listing, permissions from FTP
+
+# isolate file permissions, convert to binary (be able to send to decoder as input)
+
+# decode file permissions with binary decoder
+
+# send decoded message to output
