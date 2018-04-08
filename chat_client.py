1be37b3b176f4f71d634d88930e1a27f5dabe624
Samantha Santiago <26655231+DJSanti@users.noreply.github.com>
2018-04-08 16:54:33 -0500

    Chat Timing
    This is just more of a starter, with a list of objectives to complete and some components that we'll need to use. If previous programs are used as reference, then most of this program should come together easily. For now, just start on getting the basics to work until Gourd's site comes back up.

---
+++ chat_client.py
@@ -0,0 +1,18 @@
+# objectives:
+
+# be able to connect to chat server
+from ftplib import FTP
+# receive overt message and display to stdout
+
+# time delays between characters of overt messages
+# uses two most frequent delays to map to 0 and 1
+# decode the binary we've gotten
+# display covert message to stdout
+
+
+
+
+
+# things needed for this to work:
+# iptables - gets times for chat timing
+# need to use binary decoder as well
