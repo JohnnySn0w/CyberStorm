1be908df5df6f229881c237d07134f21b96796b6
Jonathan Ruppel <sdcheetahs@yahoo.com>
2018-04-27 10:28:21 -0500

    Removed testing cases, fixed md5 hash conversion, added more comments

--- timelock.py
+++ timelock.py
@@ -1,28 +1,52 @@
-from datetime import datetime
-import time
-from time import mktime
-import hashlib
+#!/usr/bin/env python3
+
+######################################################
+# Group: Greed
+# Members: Alexandra Duran Chicas, Zachary Guillot, 
+# Conan Howard, Oluwatoyosi Kade, Michael Mahan, 
+# Darryl Rayborn, Jonathan Ruppel, Samantha Santiago
+# Date: 04/26/2018
+# Program: timelock.py
+# Objective: Implement the following timelock algorithm:
+# calculate the time seconds elapsed of the current 
+# system time since an epoch time, compute the 
+# MD5(MD5(time elapsed)), extract and concatenate the
+# first two letters of the hash from left-to-right 
+# followed by the first two single-digit integers of the
+# hash from right-to-left, and output this "code" with
+# each one being valid for 60 seconds 
+#####################################################
+
 import sys
+import time
+import hashlib
+from time import mktime 
+from datetime import datetime
 
 letters = ""
 numbers = ""
 result = ""
+#read the epoch from stdin 
 epoch = str(sys.stdin.readline()).strip().split()
-now = "2013 05 06 07 43 25".split()
-#Note: both epoch and current times are converted to utc to account for daylight savings  
-#convert epoch time to POSIX then to utc
+
+#Note: both epoch and current times are converted to utc to account for daylight savings
+#Also, both epoch and current times are formatted YYYY MM DD HH mm SS
+
+#epoch time is converted to POSIX then to utc time
 epoch_time = datetime.utcfromtimestamp(time.mktime(datetime(int(epoch[0]), int(epoch[1]), int(epoch[2]), int(epoch[3]), int(epoch[4]), int(epoch[5])).timetuple()))
 
-#current utc time
-#now = datetime.utcnow().replace(microsecond=0)
-now_time = datetime.utcfromtimestamp(time.mktime(datetime(int(now[0]), int(now[1]), int(now[2]), int(now[3]), int(now[4]), int(now[5])).timetuple()))
+#current utc time without microseconds
+now = datetime.utcnow().replace(microsecond=0)
 
-#subtract epoch time from current then convert to POSIX
-delta_time = int((now_time - epoch_time).total_seconds())
+#calculate time elapsed by subtracting epoch time from current then convert to POSIX
+delta_time = (now - epoch_time).total_seconds()
+
+#change time elapsed such that each new code is valid for 60 seconds
+interval = delta_time % 60
+delta_time -= interval
 
 #first md5 hash conversion
-hash_time = hashlib.md5(bytes(delta_time)).hexdigest()
-
+hash_time = hashlib.md5(str(delta_time).encode()).hexdigest()
 #second md5 hash conversion
 double_hash_time = hashlib.md5(hash_time.encode()).hexdigest()
 
@@ -45,10 +69,5 @@ numbers = reversed_numbers[:2]
 #concatenate letters and numbers
 result = letters + numbers
 
-#print(str(now_time))
-#print(str(epoch_time))
-#print(delta_time)
-#print(double_hash_time)
+#send the 4-character code stdout
 print(result)
-~                                                                                                                                                                                                                                                                               
-~                           
